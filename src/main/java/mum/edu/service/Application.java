b8056be27e5399dee23755d546881ec3d7f91f25
Achyut Devkota <achyut.dev@gmail.com>
2016-05-02 15:52:30 -0500

    Database update

--- src/main/java/mum/edu/service/Application.java
+++ src/main/java/mum/edu/service/Application.java
@@ -1,5 +1,26 @@
 package mum.edu.service;
 
-public class Application {
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
+import org.apache.log4j.Logger;
+
+
+public class Application {
+	private static Logger logger = Logger.getLogger(Application.class);;
+
+	private static final EntityManagerFactory emf;
+
+	static {
+		try {
+			emf = Persistence.createEntityManagerFactory("movieImdb");
+		} catch (Throwable ex) {
+			ex.printStackTrace();
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+	
+	public static void main(String[] args){
+		
+	}
 }
