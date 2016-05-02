6b6320be063c230a7eb00b4b3c3f3e6ec2108e34
Achyut Devkota <achyut.dev@gmail.com>
2016-05-02 16:05:02 -0500

    config fix

--- src/main/java/mum/edu/service/Application.java
+++ src/main/java/mum/edu/service/Application.java
@@ -1,6 +1,8 @@
 package mum.edu.service;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 import org.apache.log4j.Logger;
@@ -13,7 +15,7 @@ public class Application {
 
 	static {
 		try {
-			emf = Persistence.createEntityManagerFactory("movieImdb");
+			emf = Persistence.createEntityManagerFactory("movieimdb");
 		} catch (Throwable ex) {
 			ex.printStackTrace();
 			throw new ExceptionInInitializerError(ex);
@@ -21,6 +23,10 @@ public class Application {
 	}
 	
 	public static void main(String[] args){
-		
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		tx.commit();
+//		
 	}
 }
